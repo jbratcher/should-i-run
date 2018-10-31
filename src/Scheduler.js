56a9d3d1baeaf3f9139309013a314e5599d642fe
jbratcher <jeremybratcher@gmail.com>
2018-10-30 21:55:43 -0400

    scheduler date picker select and logic, need dynamic detection of index in data array that matches selected day

--- src/Scheduler.js
+++ src/Scheduler.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { darkskyApiKey } from './secrets';
+import ScoreData from './main/ScoreData';
 import Header from './header';
 import Footer from './Footer';
 
@@ -8,9 +9,22 @@ class Scheduler extends Component {
   constructor(props) {
     super(props);
 
+    this.d = new Date();
+
+    this.days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ]
+
     this.state = {
-      bestDay: '',
+      bestDay: null,
       bestDayFound: false,
+      currentDayName: this.days[this.d.getDay()],
       currentLat: 38.2527,
       currentLng: -85.7585,
       forcastHumidity: [],
@@ -19,27 +33,29 @@ class Scheduler extends Component {
       forcastTime: [],
       forcastUV: [],
       forcastPrecipProbability: [],
-      forcastWeatherScores: []
+      forcastWeatherScores: [],
+      isDaySelected: false,
+      selectedDayName: this.days[this.d.getDay()],
     };
   }
-  
+
   findBestDayToRun = () => {
-    
+
     const {
       forcastTime,
       forcastWeatherScores
     } = this.state;
-    
+
     let index = forcastWeatherScores.indexOf(Math.max(...forcastWeatherScores));
-    
+
     let date = new Date(forcastTime[index] * 1000);
-    
+
     let convertedDate = new Date(date);
-    
+
     let bestDayIndex = convertedDate.getDay();
-    
+
     let bestDay =
-    
+
       bestDayIndex === 0
       ? 'Sunday'
       : bestDayIndex === 1
@@ -55,25 +71,24 @@ class Scheduler extends Component {
       : bestDayIndex === 6
       ? 'Saturday'
       : null;
-      
+
       this.setState({
         bestDay: bestDay,
         bestDayFound: true
       });
-      
+
   }
-    
-  
+
   calcuateWeatherScoresByDay = () => {
-    
-    const { 
-      forcastHumidity, 
-      forcastPrecipProbability, 
+
+    const {
+      forcastHumidity,
+      forcastPrecipProbability,
       forcastTempHigh,
       forcastTempLow,
       forcastUV,
     } = this.state;
-    
+
     const scoresArray = [];
 
     let medianTemp = 55;
@@ -81,9 +96,9 @@ class Scheduler extends Component {
     let tempScale = 10;
     let uvScale = 10;
     let rainScale = 10;
-    
+
     for(let i=0; i <=7; i++) {
-      
+
       let humidityScore = forcastHumidity[i] * 10;
       let averagedTemp = (forcastTempHigh[i] + forcastTempLow[i]) / 2;
       let tempScore = tempScale - ((Math.abs(medianTemp - averagedTemp)) / stdDevTemp);
@@ -91,20 +106,20 @@ class Scheduler extends Component {
       let rainScore = (rainScale - (forcastPrecipProbability[i] * 10));
 
       let totalScore = ((tempScore + uvScore + humidityScore + rainScore) / 4);
-      
+
       scoresArray.push(totalScore);
-      
+
     }
-    
+
     this.setState({
         forcastWeatherScores: scoresArray
       });
-    
+
     this.findBestDayToRun();
-    
+
   }
-  
-  
+
+
   fetchForcast = () => {
 
     const {currentLat, currentLng} = this.state;
@@ -129,61 +144,125 @@ class Scheduler extends Component {
 
   }
 
+  fetchForcastBySelectedDay = () => {
+
+    const {currentLat, currentLng} = this.state;
+
+    fetch(`https://calm-refuge-25215.herokuapp.com/https://api.darksky.net/forecast/${darkskyApiKey}/${currentLat},${currentLng}`)
+      .then(res => res.json())
+      .then(parsedJSON => {
+        console.log(parsedJSON);
+        this.setState({
+          forcastHumidity: parsedJSON.daily.data[0].humidity,
+          forcastPrecipProbability: parsedJSON.daily.data[0].precipProbability,
+          forcastTempHigh: parsedJSON.daily.data[0].apparentTemperatureHigh,
+          forcastTempLow: parsedJSON.daily.data[0].apparentTemperatureLow,
+          forcastUV: parsedJSON.daily.data[0].uvIndex,
+          forcastIsRaning: parsedJSON.daily.data[0].precipProbability,
+          forcastTime: parsedJSON.daily.data[0].sunriseTime,
+          isDaySelected: true
+        });
+        this.calcuateWeatherScoresByDay();
+        console.log(this.state);
+      })
+      .catch(error => console.log(error));
+
+  }
+
+  handleDayChange = (e) => {
+    e.preventDefault();
+
+    this.setState({
+      selectedDayName: e.target.value
+    })
+
+    this.fetchForcastBySelectedDay();
+
+  }
+
+  componentDidMount() {
+    console.log(this.state)
+  }
+
   render() {
-    
+
     const {
       bestDay,
-      bestDayFound
+      bestDayFound,
+      isDaySelected,
+      selectedDayName
     } = this.state;
-      
+
     return(
-        
+
         <section id="schedulerSection">
-        
-            <Header 
+
+            <Header
                 getLocationData={this.getLocationData}
             />
-            
+
             <section id="scheduler" className="column">
-            
+
                 <h2>Scheduler</h2>
-                
+
                 <p>Pick a day to get information or find the best day in the next week</p>
-                
+
                 <label>Pick a day of the week</label>
-                <select>
-                    <option value="monday">Monday</option>
-                    <option value="tuesday">Tuesday</option>
-                    <option value="wednesday">Wednesday</option>
-                    <option value="thursday">Thursday</option>
-                    <option value="friday">Friday</option>
-                    <option value="saturday">Saturday</option>
-                    <option value="sunday">Sunday</option>
+                <select value={selectedDayName} onChange={this.handleDayChange}>
+                  <option value="Sunday">Sunday</option>
+                  <option value="Monday">Monday</option>
+                  <option value="Tuesday">Tuesday</option>
+                  <option value="Wednesday">Wednesday</option>
+                  <option value="Thursday">Thursday</option>
+                  <option value="Friday">Friday</option>
+                  <option value="Saturday">Saturday</option>
                 </select>
-                
+
+                {
+                  // {isDaySelected ?
+                  //
+                  //   <ScoreData
+                  //
+                  //     airQuality={airQuality}
+                  //     currentHumidity={currentHumidity}
+                  //     currentTemp={currentTemp}
+                  //     currentUV={currentUV}
+                  //     currentWeatherIcon={currentWeatherIcon}
+                  //     currentWeatherSummary={currentWeatherSummary}
+                  //     data={data}
+                  //     mainPollutant={mainPollutant}
+                  //     userTempScale={userTempScale}
+                  //     weatherScore={weatherScore}
+                  //
+                  //   />
+                  //
+                  // : null
+                  // }
+                }
+
                 <label>Find the best day of the week</label>
                 <button
                   onClick={this.fetchForcast}
                 >
                   Go
                 </button>
-                
+
                 {bestDayFound ?
-                
+
                   <p>
                     {`The best day to run is ${bestDay}`}
                   </p>
-                  
+
                 : null
-                
+
                 }
-            
+
             </section>
-            
+
             <Footer / >
-        
+
         </section>
-          
+
     );
 
   }
