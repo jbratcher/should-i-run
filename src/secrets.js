ef71886c5fce07f4ce9d554b1f5894904beefee3
jbratcher <jeremybratcher@gmail.com>
2018-11-15 18:11:07 -0500

    added apiKeys for project review

---
+++ src/secrets.js
@@ -0,0 +1,13 @@
+// api key
+
+/*http://api.airvisual.com/v2/city?city=Louisville&state=Kentucky&country=USA&key=vg9z3taLddFpyTbAa*/
+
+export const apiKey = 'vg9z3taLddFpyTbAa';
+
+export const gmapsApiKey = 'AIzaSyAGNAWHYmzPtl9h2vVMmLBhlD5o3Et1NCw';
+
+export const darkskyApiKey = '7caf9128d134c8a87482de7629abebdf';
+
+// dark sky example call
+
+// `https://api.darksky.net/forecast/${darkskyApiKey}/[latitude],[longitude]`
